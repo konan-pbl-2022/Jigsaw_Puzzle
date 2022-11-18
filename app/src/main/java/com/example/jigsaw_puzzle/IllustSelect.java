a8d736dd82e491353272b15f164fc8cbdbf7eb41
ShiomuraKazuki <sol-kazu@outlook.jp>
2022-11-18 17:47:14 +0900

    IllustSelectの画像付きボタンの作成、および画面遷移のみ作りました。

--- app/src/main/java/com/example/jigsaw_puzzle/IllustSelect.java
+++ app/src/main/java/com/example/jigsaw_puzzle/IllustSelect.java
@@ -2,7 +2,11 @@ package com.example.jigsaw_puzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class IllustSelect extends AppCompatActivity {
 
@@ -10,5 +14,51 @@ public class IllustSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_illust_select);
+
+        ImageButton Gazounext1 = (ImageButton)findViewById(R.id.yuyake);
+        ImageButton Gazounext2 = (ImageButton)findViewById(R.id.yama);
+        ImageButton Gazounext3 = (ImageButton)findViewById(R.id.syasin);
+        ImageButton Gazounext4 = (ImageButton)findViewById(R.id.tenkuusima);
+        Button backButton = (Button)findViewById(R.id.back);
+
+        Gazounext1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(IllustSelect.this,PlayPuzzle.class);
+                startActivity(intent);
+            }
+        });
+
+        Gazounext2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(IllustSelect.this,PlayPuzzle.class);
+                startActivity(intent);
+            }
+        });
+
+        Gazounext3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(IllustSelect.this,PlayPuzzle.class);
+                startActivity(intent);
+            }
+        });
+
+        Gazounext4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(IllustSelect.this,PlayPuzzle.class);
+                startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(IllustSelect.this,PlaySelect.class);
+                startActivity(intent);
+            }
+        });
     }
 }
