9e202c3ab2eac1f6bd874f338226b4fa7166a81f
daichi-yamamoto-konan <s2071144@s.konan-u.ac.jp>
2022-11-25 13:55:56 +0900

    Merge branch 'main' into canvas_setup
    # Conflicts:
    #	app/src/main/res/layout/activity_play_puzzle.xml

--- app/src/main/java/com/example/jigsaw_puzzle/PlayPuzzle.java
+++ app/src/main/java/com/example/jigsaw_puzzle/PlayPuzzle.java
@@ -1,9 +1,12 @@
 package com.example.jigsaw_puzzle;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +20,15 @@ public class PlayPuzzle<Graphics> extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_puzzle);
+
+        Button Back = (Button)findViewById(R.id.back_button);
+        Back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PlayPuzzle.this,Title.class);
+                startActivity(intent);
+            }
+        });
 
         //画像の取得
         Resources resources = getResources();
